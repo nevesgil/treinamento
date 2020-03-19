073ceba2de866cca538039c92c147d65e8e4a589
Gilmar Neves <gilmarneves@outlook.com>
2020-03-19 11:06:53 -0300

    nova modificacao

--- clouds.py
+++ clouds.py
@@ -1,7 +1,13 @@
 """
 Clouds classes - Python
 Desenvolvido em 21/10/2015
+<<<<<<< HEAD
 Por Doc Brown & Marty McFly
+||||||| 5484f22... Inclusao de objeto azure
+Por Doc Brown
+=======
+Por Marty McFly
+>>>>>>> parent of 5484f22... Inclusao de objeto azure
 """
 
 class Clouds(object):
@@ -18,8 +24,17 @@ class Clouds(object):
         self.name = newname
     def __str__(self):
         return "Cloud: "+str(self.name)+" - Created on: "+str(self.year)
+<<<<<<< HEAD
 
 aws = Clouds()
 aws.set_name("Amazon Web Services")
 aws.set_year("2006")
 print(aws)
+||||||| 5484f22... Inclusao de objeto azure
+
+azure = Clouds()
+azure.set_name("Microsoft Azure")
+azure.set_year("2010")
+print(azure)
+=======
+>>>>>>> parent of 5484f22... Inclusao de objeto azure
